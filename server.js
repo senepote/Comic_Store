bb6f7d31a50efe4b49bccf42e80db1c62eefa309
STEVE NEPOTE <senepote@gmail.com>
2018-07-06 09:46:42 -0500

    listening on 3000

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('testing');
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=>{
+  res.send('testing')
+})
+
+app.listen(3000, ()=>{
+  console.log('listening...');
+})
