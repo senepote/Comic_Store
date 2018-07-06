cc98a0d380abbcd0dcf5b3608e21e458f6f0a833
STEVE NEPOTE <senepote@gmail.com>
2018-07-06 10:51:39 -0500

    mongoose added and connected

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const mongoose = require('mongoose');
+const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
+
+
 app.get('/', (req,res)=>{
   res.send('testing')
 })
 
 app.listen(PORT, ()=>{
   console.log('listening...');
+})
+
+
+mongoose.connect(mongoUri);
+mongoose.connection.on('open', ()=>{
+  console.log('mongoose connected!!!!!!!');
 })
