7456370252b5c33bc02708c1d0e5430810aaca65
STEVE NEPOTE <senepote@gmail.com>
2018-07-07 12:42:44 -0500

    attempting to synch up router

--- controllers/comics.js
+++ controllers/comics.js
@@ -3,14 +3,101 @@ const router = express.Router();
 const Comics = require('../models/comics.js');
 
 //SEED
+// router.get('/seed', (req,res)=>{
+//   Comic.create(
+//     [
+//       {
+//         name: 'X-Men #1 (1963)',
+//         description: 'The classic that started it all...',
+//         img:'https://vignette.wikia.nocookie.net/marvel/images/7/77/X-Men_-1.jpg/revision/latest?cb=20121011154900&path-prefix=fr',
+//         price: 300,
+//         qty: 1
+//       },{
+//         name:
+//         description:
+//         img:
+//         price:
+//         qty:
+//       },{
+//         name:
+//         description:
+//         img:
+//         price:
+//         qty:
+//       },{
+//         name:
+//         description:
+//         img:
+//         price:
+//         qty:
+//       },{
+//         name:
+//         description:
+//         img:
+//         price:
+//         qty:
+//       },{
+//         name:
+//         description:
+//         img:
+//         price:
+//         qty:
+//       },{
+//         name:
+//         description:
+//         img:
+//         price:
+//         qty:
+//       },{
+//         name:
+//         description:
+//         img:
+//         price:
+//         qty:
+//       },{
+//         name:
+//         description:
+//         img:
+//         price:
+//         qty:
+//       },{
+//         name:
+//         description:
+//         img:
+//         price:
+//         qty:
+//       },{
+//         name:
+//         description:
+//         img:
+//         price:
+//         qty:
+//       }
+//     ]
+//   )
+// })
 
 
+//CREATE ROUTE
+router.get('/comics/new',(req,res)=>{
+  res.render('new.ejs');
+});
+//DELETE ROUTE
 
+//EDIT ROUTE
 
-
-
-
-
+//INDEX ROUTE
+// router.get('/',(req,res)=>{
+//   Comic.find({}, (err,allComics)=>{
+//     res.render('index.ejs', {
+//       comic: allComics
+//     });
+//   });
+// });
+router.get('/', (req,res)=>{
+  res.render('index.ejs')
+})
+//SHOW ROUTE
 
 
 
--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const methodOverride = require('method-override');
 
 const mongoose = require('mongoose');
 const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
+const db = mongoose.connection;
 
+//MIDDLEWARE
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+// app.use(express.json());
 
+const comicController = require('./controllers/comics.js');
+app.use('/comics', comicController);
+// app.get('/comics/new', (req,res)=>{
+//   res.render('new.ejs')
+// })
 app.get('/', (req,res)=>{
-  res.send('testing')
+  res.render('index.ejs')
 })
+
+
 
 app.listen(PORT, ()=>{
   console.log('listening...');
